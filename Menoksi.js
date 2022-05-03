f443517942fa64e47ea283052638019dddf2ca53
olgajoki <104416760+olgajoki@users.noreply.github.com>
2022-05-03 20:35:53 +0300

    Add files via upload

--- Menoksi.js
+++ Menoksi.js
@@ -10,7 +10,6 @@ for(let i = 1; i <= juomapelinTehtavat.length; i++){
   }, 20000 * i);
 }
 
-'use strict';
 //API, jolla haetaan Helsoingin lähialueiden tapahtumia.
 
 const apiurl = "http://api.hel.fi/linkedevents/v1/event/?division=";
@@ -105,3 +104,102 @@ function removeColor(evt){
 
 lisatietoaLinkki.addEventListener('mouseover', changeColor);
 lisatietoaLinkki.addEventListener("mouseout", removeColor);
+
+
+
+//
+
+// välityspalvelimen (AllOrigins) osoite CORS-ongelman ratkaisemiseen
+const proxy = 'https://api.allorigins.win/get?url=';
+const apiurlweather = "https://weatherdbi.herokuapp.com/data/weather/";
+
+// hakukysely, jolla dataa haetaan.
+let apiKyselyweather;
+// hakukysely, johon on lisätty vielä proxyn vaatima lisäys
+let proxyApikysely;
+
+// Etsitään HTML-sivulta tarvittavat komponentit id:n avulla.
+const hakunappiweather = document.getElementById("hakunappiweather");
+const hakutekstiweather = document.getElementById('hakutekstiweather');
+const tuloksetweather = document.querySelector('#APIhakuweather');
+
+
+// lisätään napille tapahtumankäsittelijä
+hakunappiweather.addEventListener('click', teeKyselyweather);
+
+// Funktio muodostaa hakukyselyn.
+// Lopuksi funktio kutsuu teeHaku() funktiota.
+function teeKyselyweather() {
+
+  document.querySelector('#APIhakuweather').innerHTML = '';
+
+  let valueweather = hakutekstiweather.value;
+
+  let hakusanaweather = valueweather;
+
+  // muodostetaan ja tulostetaan konsoliin lopullinen hakukysely
+  apiKyselyweather = apiurlweather + hakusanaweather;
+  console.log('normaali hakukysely: ' + apiKyselyweather);
+  // lisätään vielä proxyn osoite (CORS ongelmien ratkaisija)
+  proxyApikysely = proxy + encodeURIComponent(apiKyselyweather);
+  console.log("cors-kysely: " + proxyApikysely);
+
+  // kutsutaan fetch-jutut hoitavaa funktiota,
+  // huom: osoite sisältää nyt myös proxyn eli välityspalvelimen osoitteen
+  teeHakuweather(proxyApikysely);        // parametrina proxyn osoite + hakulause
+}
+
+function teeHakuweather(apiKyselyweather)  {
+
+  // suoritetaan hakukysely, fetch hoitaa mahdolliset tietoliikenneongelmat.
+  fetch(apiKyselyweather).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    naytaVastausweather(json);				// siirrytään varsinaisen datan käsittelyyn.
+  }).catch(function(error){           // Jos tapahtuu virhe,
+    console.log(error);             // kirjoitetaan virhe konsoliin.
+  });
+}
+
+
+
+function naytaVastausweather(proxynData) {
+
+  const jsonData = JSON.parse(proxynData.contents);
+
+  console.log("json muotoinen data sellaisenaan");
+  console.log(jsonData);
+
+  console.log("Sellainen se json oli.");
+
+
+  let htmlKoodiweather = `
+<br>
+        <h3> Paikka:</h3> ${jsonData.region}
+
+        <h4>Sää tällä hetkellä:</h4>
+            <figure>
+                <img src="${jsonData.currentConditions.iconURL}">
+            </figure>
+         <p>Asteita: ${jsonData.currentConditions.temp.c} °</p>
+         <br><h4>Sää huomenna:</h4>     
+            <figure>
+            <img src=" ${jsonData.next_days[1].iconURL}">
+            </figure>
+         <p>Asteita: ${jsonData.next_days[1].min_temp.c}° - ${jsonData.next_days[1].max_temp.c}°</p>
+
+          <br> <h4>Sää ylihuomenna:</h4>       
+            <figure>
+            <img src="${jsonData.next_days[2].iconURL}">          
+            </figure> 
+         <p>Asteita: ${jsonData.next_days[2].min_temp.c}° - ${jsonData.next_days[2].max_temp.c}°</p>
+
+
+            `;
+  tuloksetweather.innerHTML += htmlKoodiweather;
+
+
+}
+
+
+
