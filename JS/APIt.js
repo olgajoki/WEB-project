2e69fc1dc90f121554fc606b80e488891b08a323
olgajoki <104416760+olgajoki@users.noreply.github.com>
2022-05-08 17:49:10 +0300

    Add files via upload

--- JS/APIt.js
+++ JS/APIt.js
@@ -37,7 +37,7 @@ lisatietoaHakukenttaWeather.addEventListener('mouseout', removeColorWeather);
 
 
 
-//API, jolla haetaan Helsoingin lähialueiden tapahtumia ja tulostetaan ne erilliseen scroll bar boksiin.
+//API, jolla haetaan Helsingin lähialueiden tapahtumia ja tulostetaan ne erilliseen scroll bar boksiin.
 // Olga Jokinen 4.5.2022
 
 // välityspalvelimen (AllOrigins) osoite CORS-ongelman ratkaisemiseen
@@ -161,7 +161,7 @@ function teeKyselyWeather() {
     document.querySelector('#APIhakuWeather').innerHTML = '';
 
     let hakusanaWeather = hakutekstiWeather.value;
-
+    let proxyApikysely;
     // muodostetaan ja tulostetaan konsoliin lopullinen hakukysely
     apiKyselyWeather = apiurlWeather + hakusanaWeather;
     // lisätään vielä proxyn osoite (CORS ongelmien ratkaisija)
@@ -192,13 +192,13 @@ function teeHakuWeather(apiKyselyWeather)  {
 }
 
 
-//Tulostetaan json kosoliin
-function naytaVastausWeather(proxynData) {
-
-    const jsonData = JSON.parse(proxynData.contents);
+//Tulostetaan json konsoliin
+function naytaVastausWeather(proxynDataWeather) {
+    console.log('Hakusanassa ei ole vikaa, mutta koodissa \n const jsonDataWeather = JSON.parse(proxynDataWeather.contents);\n vaikuttaisi olevan. :(')
+    const jsonDataWeather = JSON.parse(proxynDataWeather.contents);
 
     console.log("json muotoinen data sellaisenaan");
-    console.log(jsonData);
+    console.log(jsonDataWeather);
     console.log("Sellainen se json oli.");
 
 //Lisätään tämän hetkinen, huomisen ja ylihuomisen säätiedot html:ään
@@ -231,3 +231,88 @@ function naytaVastausWeather(proxynData) {
     tuloksetWeather.innerHTML += htmlKoodiWeather;
 
 }
+
+// Kartta, jossa reititystoiminto
+// Olga Jokinen 8.5.2022
+
+const apiKey = "AAPKf8c49bd5910c45f2b0e3cfa504ec9557PWvltKsnoVO6Biz4wH88YOyySeuQG7d8oyoTj9qt9RfKvWnrHPQxe8pk8mdl-aGj";
+
+const basemapEnum = "ArcGIS:Navigation";
+
+const map = L.map("map", {
+    minZoom: 2
+
+}).setView([60.165249, 24.936056], 12); // Helsingin koordinaatit
+
+L.esri.Vector.vectorBasemapLayer(basemapEnum, {
+    apiKey: apiKey
+}).addTo(map);
+
+// Add a DOM Node to display the text routing directions
+const directions = document.createElement("div");
+directions.id = "directions";
+directions.innerHTML = "Klikkaa kartasta ensiksi lähtöpiste ja sitten päätepiste.";
+document.body.appendChild(directions);
+
+// Layer Group for start/end-points
+const startLayerGroup = L.layerGroup().addTo(map);
+const endLayerGroup = L.layerGroup().addTo(map);
+
+// Layer Group for route lines
+const routeLines = L.layerGroup().addTo(map);
+
+let currentStep = "start";
+let startCoords, endCoords;
+
+function updateRoute() {
+    // Create the arcgis-rest-js authentication object to use later.
+    const authentication = arcgisRest.ApiKeyManager.fromKey(apiKey);
+
+    // make the API request
+    arcgisRest
+        .solveRoute({
+            stops: [startCoords, endCoords],
+            endpoint: "https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World/solve",
+            authentication
+        })
+        .then((response) => {
+            // Show the result route on the map.
+            routeLines.clearLayers();
+            L.geoJSON(response.routes.geoJson).addTo(routeLines);
+
+            // Show the result text directions on the map.
+            const directionsHTML = response.directions[0].features.map((f) => f.attributes.text).join("<br/>");
+            directions.innerHTML = directionsHTML;
+            startCoords = null;
+            endCoords = null;
+        })
+        .catch((error) => {
+            console.error(error);
+            alert("There was a problem using the route service. See the console for details.");
+        });
+}
+
+// When the map is clicked, get the coordinates, store the start or end
+// state, and pass them to the updateRoute function which calls the REST endpoint.
+map.on("click", (e) => {
+    const coordinates = [e.latlng.lng, e.latlng.lat];
+
+    if (currentStep === "start") {
+        startLayerGroup.clearLayers();
+        endLayerGroup.clearLayers();
+        routeLines.clearLayers();
+        L.marker(e.latlng).addTo(startLayerGroup);
+        startCoords = coordinates;
+        currentStep = "end";
+    } else {
+        L.marker(e.latlng).addTo(endLayerGroup);
+        endCoords = coordinates;
+        currentStep = "start";
+
+    }
+
+    if (startCoords && endCoords) {
+        updateRoute();
+    }
+});
+
